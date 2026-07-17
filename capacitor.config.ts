c7181e5100f95d836d09db3531664f99d4087da9
szcer <szcerba@gmail.com>
2026-07-17 14:13:23 -0300

    se agrega capacitor para despliegue en android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.vivere.app',
+  appName: 'vivereapp',
+  webDir: 'dist/vivereapp/browser'
+};
+
+export default config;
